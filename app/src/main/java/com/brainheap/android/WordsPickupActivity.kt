b75f02a3b48fefde30deabb7ac5d1915b23680ca
dpotoskuev <dpotoskuev@gmail.com>
2019-05-01 23:19:43 +0100

    initial commit

---
+++ app/src/main/java/com/brainheap/android/WordsPickupActivity.kt
@@ -0,0 +1,12 @@
+package com.brainheap.android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WordsPickupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_words_pickup)
+    }
+}
