f05cb651c0914beed95f220e8490443dc00bce1e
dpotoskuev <dpotoskuev@gmail.com>
2019-05-02 22:46:29 +0100

    Text selection action

--- app/src/main/java/com/brainheap/android/WordsPickupActivity.kt
+++ app/src/main/java/com/brainheap/android/WordsPickupActivity.kt
@@ -2,11 +2,31 @@ package com.brainheap.android
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.TextView
+
+
 
 class WordsPickupActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_words_pickup)
+
+        handleIntent()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        handleIntent()
+    }
+
+    private fun handleIntent() {
+        val selectedTextView:TextView = findViewById(R.id.selectedTextView)
+        val text = intent
+            .getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)
+        selectedTextView.text = text
     }
 }
