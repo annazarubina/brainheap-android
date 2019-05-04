c3292bd33484c657141f4b3227c7ed53abf9bdf9
dpotoskuev <dpotoskuev@gmail.com>
2019-05-04 20:17:16 +0100

    Words pickup activiy imporrvements

--- app/src/main/java/com/brainheap/android/WordsPickupActivity.kt
+++ app/src/main/java/com/brainheap/android/WordsPickupActivity.kt
@@ -1,6 +1,6 @@
 package com.brainheap.android
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.graphics.Color
